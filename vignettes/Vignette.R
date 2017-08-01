e4bb3eb79554c6206b6a868316e150382e1322fb
VP Nagraj <vpnagraj@virginia.edu>
2017-08-01 12:05:09 -0400

    removed old version of vignette and extra files

--- vignettes/Vignette.R
+++
@@ -1,7 +0,0 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
-
-## ---- echo=TRUE----------------------------------------------------------
-library(hei)
-library(tidyverse)
-
