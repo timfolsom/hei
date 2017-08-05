8a2fd2d02387eb2a7de1f5a43a906a15dc55000b
VP Nagraj <vpnagraj@virginia.edu>
2017-08-05 11:28:06 -0400

    get_diet no longer defaults to both days

--- R/get_diet.R
+++ R/get_diet.R
@@ -3,7 +3,7 @@
 #' \code{get_diet} returns an abridged National Health and Nutrition Examination Survey dietary data set for a given year.
 #'
 #' @param year year combination of data set to retrieve must be one of "2005/2006", "2007/2008", "2009/2010", "2011/2012" or "2013/2014"
-#' @param day identifier for survey day to retrieve ... must be one of "first", "second" or "both" (default is set to "both")
+#' @param day identifier for survey day to retrieve ... must be one of "first", "second" or "both"
 #' @return Object of class \code{data.frame} containing a modified NHANES dietary data set for a specified year and day. The complete NHANES dietary data set contains several more variables than are necessary for computing HEI scores. The data set returned by this function has been trimmed down to contain (in addition to an NHANES unique sequence identifier for each participant) only the columns of data needed for HEI score calculation, specifically:
 #' \itemize{
 #' \item TKCAL: Energy (kcal)
@@ -20,7 +20,7 @@
 #' get_diet("2009/2010", "first")
 #' }
 
-get_diet <- function(year, day = "both") {
+get_diet <- function(year, day) {
 
     yearchoices <- c("D" = "2005/2006",
                      "E"= "2007/2008",
