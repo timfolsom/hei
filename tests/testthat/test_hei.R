f29b5db8a837f598b445d05b3c06f7357193197d
VP Nagraj <vpnagraj@virginia.edu>
2017-08-11 16:57:45 -0400

    one more test for verbose = T hei arg

--- tests/testthat/test_hei.R
+++ tests/testthat/test_hei.R
@@ -6,8 +6,20 @@ test_that("HEI score is same as original validation data", {
     fped <- get_fped("2009/2010", "both")
     demog <- get_demo("2009/2010")
 
-    heires <- hei(diet,fped,demog)
+    heires <- hei(fped,diet,demog)
 
     expect_equal_to_reference(heires$HEI, file = "rds/valid.rds")
+
+})
+
+test_that("HEI verbose argument returns all columns", {
+
+    diet <- get_diet("2009/2010", "both")
+    fped <- get_fped("2009/2010", "both")
+    demog <- get_demo("2009/2010")
+
+    heires <- hei(fped,diet, demog, verbose = TRUE)
+
+    expect_equal(ncol(heires), 70)
 
 })
